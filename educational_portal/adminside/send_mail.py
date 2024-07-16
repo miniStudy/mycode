b31d8f7acd3ba4ffa21803ab997a38c49d73c8cf
[anilkathayat07] <[kathayatanil3@gmail.com]>
2024-07-16 15:28:16 +0530

    changes by studentside

--- educational_portal/adminside/send_mail.py
+++ educational_portal/adminside/send_mail.py
@@ -17,3 +17,18 @@ def announcement_mail(title,msg,list_of_receivers):
     msg = EmailMultiAlternatives(sub, text_content, email_from, recp_list)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+
+def timetable_mail(list_of_receivers):
+    sub = 'New Update from miniStudy'
+    title = 'Time Table Updated'
+    msg = 'Your time table has been updated!'
+    email_from = 'miniStudy <mail@ministudy.in>'
+    recp_list = list_of_receivers
+    htmly = get_template('Email/timetable.html')
+    d = {'title':title, 'msg':msg}
+    text_content = ''
+    html_content = htmly.render(d)
+    msg = EmailMultiAlternatives(sub, text_content, email_from, recp_list)
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
