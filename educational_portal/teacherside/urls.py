5baa611b5f731eb9a307bef765ba8ea448796a61
[TrushalPatel0] <[business.trushalpatel@gmail.com]>
2024-07-16 15:54:58 +0530

    changes done for teacherside

--- educational_portal/teacherside/urls.py
+++ educational_portal/teacherside/urls.py
@@ -15,18 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from adminside import views as adminview
+from teacherside import views as teacherview
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('adminside/', include('adminside.urls')),
-
-    path('summernote/', include('django_summernote.urls')),
-    path('studentside/', include('studentside.urls')),
-    path('teacherside/', include('teacherside.urls')),
+    path('', teacherview.teacher_home, name='teacher_home'),
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
