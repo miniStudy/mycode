c8fa7bc3095e1add26b807fb704f80a4d0d62e0a
Shivam Kumar Sharma <shivksharma0904@gmail.com>
2024-08-12 17:32:08 +0530

    Changes Shivam

--- educational_portal/api/views.py
+++ educational_portal/api/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from .models import *
+from .serializers import * 
+from rest_framework import status 
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view(['GET'])
+def get_boards(request):
+    result = Boards.objects.all()
+    apidata = BoardsSerializer(result, many=True)
+    print(apidata)
+    return Response ({
+        'data': apidata.data,
+        'message' : 'Boards',
+    })
+
+@api_view(['POST', 'GET'])  
+def api_update_boards(request):
+    if request.GET.get('pk'):
+        pk = request.GET['pk']
+        instance = get_object_or_404(Boards, pk=pk)
+        if request.method == "POST":
+            form = BoardsSerializer(request.POST, instance=instance)
+            if form.is_valid():
+                form.save()
+                return Response({
+                'status': True,
+                'message': 'Update success',
+                'data': form.data
+            })
+            else:
+                filled_data = form.data
+                return render(request, 'insert_update/boards.html', {'errors': form.errors,'filled_data':filled_data})
+    
+    if request.GET.get('update_id'):
+        update_id = request.GET['update_id']
+        update_data = Boards.objects.get(brd_id = update_id)
+        context2 = {
+            'update_data' : update_data
+
+        }
+        context.update(context2)
+        return render(request, 'insert_update/boards.html',context)
+
+    if request.method == "POST":
+        form = brd_form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('boards')
+        else:
+            filled_data = form.data
+            print(filled_data)
+            return render(request, 'insert_update/boards.html', {'errors': form.errors,'filled_data':filled_data})
+    return render(request, 'insert_update/boards.html',context)
+    
+
+@api_view(['DELETE', 'GET'])    
+def delete_boards(request):
+    id = request.GET.get('id')
+    result = get_object_or_404(Boards, brd_id=id)  
+    result.delete()
+    return Response({"status": "success", "data": "Record Deleted"})
+    
+
