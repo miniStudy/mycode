2b23739039e7f4fe965533aab77980d4bce7da89
[anilkathayat07] <[kathayatanil3@gmail.com]>
2024-08-13 13:53:32 +0530

    changes

---
+++ datasets.py
@@ -0,0 +1,23 @@
+# (10 standard subjects data)
+# INSERT INTO `subject`(`sub_name`, `sub_std_id`) VALUES ('Maths',13),
+# ('Science',13),
+# ('Social Science',13),
+# ('History',13),
+# ('Geography',13),
+# ('Economics',13),
+# ('Political Science',13);
+
+
+
+# (9 standard subjects data)
+# INSERT INTO `subject`(`sub_name`, `sub_std_id`) VALUES ('Maths',12),
+# ('Science',12),
+# ('Social Science',12),
+# ('History',12),
+# ('Geography',12),
+# ('Economics',12),
+# ('Political Science',12);
+
+
+# (9 standard chapters data)
+# INSERT INTO `chepter`(`chep_name`, `chep_sem`, `chep_icon`, `chep_std_id`, `chep_sub_id`) VALUES ('Number Systems','sem1','','12','19'), ('Polynomials','sem1','','12','19'), ('Coordinate Geometry','sem1','','12','19'), ('Linear Equations in Two Variables','sem1','','12','19'), ('Euclid’s Geometry','sem1','','12','19'), ('Lines and Angles','sem1','','12','19'), ('Triangles','sem1','','12','19'), ('Quadrilaterals','sem1','','12','19'), ('Areas of Parallelograms and Triangles','sem1','','12','19'), ('Circles','sem1','','12','19'), ('Constructions','sem1','','12','19'), ('Heron’s Formula','sem1','','12','19'), ('Surface Areas and Volumes','sem1','','12','19'), ('Statistics','sem1','','12','19'), ('Probability','sem1','','12','19');
