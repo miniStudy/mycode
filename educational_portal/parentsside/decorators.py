dc4ec47fe7a0b4ca064f8d4f7fd5b10380c92be5
[anilkathayat07] <[kathayatanil3@gmail.com]>
2024-09-04 11:51:34 +0530

    changes

--- educational_portal/parentsside/decorators.py
+++ educational_portal/parentsside/decorators.py
@@ -6,7 +6,7 @@ def parent_login_required(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
         if 'parent_logged_in' not in request.session:
-            messages.error(request, "<div class='bg-danger text-white p-2 rounded-2 returnmessage mb-2' id='returnmessage'><i class='fa-solid fa-triangle-exclamation me-2'></i> Login Required.</div>")
+            messages.error(request, "Login Required.")
             return redirect('parent_login')
         return view_func(request, *args, **kwargs)
     return wrapper
