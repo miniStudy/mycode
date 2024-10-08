3178d4d5695a57f5fe8666b40e1b355d4f5daef3
anilkathayat07 <kathayatanil3@gmail.com>
2024-10-08 16:53:43 +0530

    Changes

--- educational_portal/teacherside/send_mail.py
+++ educational_portal/teacherside/send_mail.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.template import Context
+from datetime import datetime
 from django.core.mail import EmailMultiAlternatives
 
 def attendance_student_present_mail(status,date,list_of_receivers):
@@ -69,3 +70,57 @@ def timetable_mail(list_of_receivers):
     msg = EmailMultiAlternatives(sub, text_content, email_from, recp_list)
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+# def marks_mail(marks, email_ids):
+#     sub = 'Test Marks Result'
+#     email_from = 'miniStudy <mail@ministudy.in>'
+#     htmly = get_template('teacherpanel/Email/marks_student.html')
+    
+#     for i, email in enumerate(email_ids):
+#         student_marks = marks[i]
+#         d = {
+#             'title': 'Test Marks Notification',
+#             'msg': f'Your test marks are: {student_marks}',
+#         }
+#         text_content = ''
+#         html_content = htmly.render(d)
+#         msg = EmailMultiAlternatives(sub, text_content, email_from, [email])
+#         msg.attach_alternative(html_content, "text/html")
+#         msg.send()
+
+
+from django.core.mail import EmailMultiAlternatives, get_connection
+from django.template.loader import get_template
+
+def marks_mail(marks, email_ids, test_name, total_marks, test_date):
+    sub = f'Test Marks Result for {test_name}'
+    email_from = 'miniStudy <mail@ministudy.in>'
+    htmly = get_template('teacherpanel/Email/marks_student.html')
+
+    connection = get_connection()
+
+    messages = []
+
+    if isinstance(test_date, str):
+        formatted_date = test_date
+    else:
+        formatted_date = test_date.strftime('%d-%m-%Y')
+
+    for i, email in enumerate(email_ids):
+        student_marks = marks[i]
+        print(student_marks)
+        print(email)
+        d = {
+            'title': 'Test Marks Notification',
+            'test_name': test_name,
+            'total_marks': total_marks,
+            'test_date': formatted_date,
+            'student_marks': student_marks,
+        }
+        html_content = htmly.render(d)
+        
+        msg = EmailMultiAlternatives(sub, '', email_from, [email])
+        msg.attach_alternative(html_content, "text/html")
+      
+        messages.append(msg)
+    connection.send_messages(messages)
--- educational_portal/teacherside/views.py
+++ educational_portal/teacherside/views.py
@@ -688,6 +688,7 @@ def teacher_insert_offline_marks(request):
         batch_id = Batches.objects.get(batch_id=batch_id)
         context.update({'students_data':students_data, 'batch_id':batch_id})
     
+    
     return render(request, 'teacherpanel/offline_marks.html',context)
 
 @teacher_login_required
@@ -699,6 +700,8 @@ def teacher_save_offline_marks(request):
         date = request.POST.get('tau_date')
         test_data = Test_questions_answer.objects.filter(tq_name__test_id = test_id)
         test_id = Chepterwise_test.objects.get(test_id=test_id)
+        
+
         sum = 0
         count = 0
         for x in test_data:
@@ -718,6 +721,29 @@ def teacher_save_offline_marks(request):
                 tau_date = date,
             )
             test_attempt.save()
+
+        email_ids = []
+        student_marks = []
+
+        if not date:
+            date = datetime.now().date() 
+            
+        test = Test_attempted_users.objects.filter(tau_test_id__test_id=test_id.test_id).first()
+
+        if test:
+            test_name = test.tau_test_id.test_name
+            total_marks = test.tau_total_marks
+            
+
+            for i, stud_id in enumerate(student_ids):
+                student_email = Students.objects.get(stud_id=stud_id)
+                email_ids.append(student_email.stud_email)
+                student_marks.append(marks[i])
+
+            marks_mail(student_marks, email_ids, test_name, total_marks, date)
+        else:
+            print("No test found for the given test ID.")
+
     messages.success(request, 'Marks have been successfully saved.')
     return redirect('teacher_test')
 
