b7eec139fc40025764f5a32ef0478d28b4ba6300
Shivxsks <shivksharma0904@gmail.com>
2024-10-17 18:20:58 +0530

    Changes

--- educational_portal/adminside/views.py
+++ educational_portal/adminside/views.py
@@ -1870,12 +1870,22 @@ def delete_students(request):
 @admin_login_required
 def show_inquiries(request):
     domain = request.get_host()
-    title = "Inquiries"
+    title = "Leads"
     inquiries_data = Inquiries.objects.filter(domain_name = domain)
+    total_inquiries = inquiries_data.count()
+    students_email = Students.objects.filter(domain_name = domain).values('stud_email')
+    matching_inquiries = Inquiries.objects.filter(domain_name = domain, inq_email__in=students_email)
+    total_conversion = matching_inquiries.count()
+    percentage = round((total_conversion/total_inquiries)*100,2)
+
 
     context = {
         "title":title,
-        "inquiries_data":inquiries_data
+        "inquiries_data":inquiries_data,
+        "total_inquiries":total_inquiries,
+        "total_conversion":total_conversion,
+        "matching_inquiries":matching_inquiries,
+        "percentage":percentage
     }
     return render(request, 'show_inquiries.html', context)
 
