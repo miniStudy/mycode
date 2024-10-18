65d6447e9e363f9365820a52f52ddb7a589175aa
[TrushalPatel0] <[business.trushalpatel@gmail.com]>
2024-10-18 11:20:17 +0530

    changes done

--- educational_portal/website/views.py
+++ educational_portal/website/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def firstpage(request):
+    context={}	
+
+    if request.GET.get("deviceId"):
+        deviceId = request.GET.get('deviceId','1')
+        if deviceId != '123':
+            request.session['deviceId'] = deviceId
+        context.update({'deviceId':deviceId})
+
     return render(request, "pages/index.html")
 
