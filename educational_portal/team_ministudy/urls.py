b2bff33615d554e75f34505976cf9d7dbbb436b8
anilkathayat07 <kathayatanil3@gmail.com>
2024-10-28 17:30:58 +0530

    Changes

--- educational_portal/team_ministudy/urls.py
+++ educational_portal/team_ministudy/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from team_ministudy import views as ministudyview
 
 urlpatterns = [
+    path('', ministudyview.team_ministudy_home, name='home'),
+
     path('show_institute/', ministudyview.show_institute_function, name='show_institute'),
     path('insert_update_institute/', ministudyview.insert_update_institute_function, name='insert_update_institute'),
 
@@ -10,8 +12,12 @@ urlpatterns = [
 
     path('institute_lock', ministudyview.institute_lock_function, name='institute_lock'),
 
-
-
     path('remove_institute', ministudyview.remove_institute_function, name='remove_institute'),
+
+
+    path('bulk_upload_test_questions/', ministudyview.bulk_upload_questions, name='bulk_upload_test_questions'),
+    path('show_question_bank/', ministudyview.show_question_bank, name="show_question_bank"),
+    path('edit_question_bankk/', ministudyview.edit_question_bankk, name="edit_question_bankk"),
+    path('delete_question_bank/', ministudyview.delete_question_bank, name="delete_question_bank"),
 ]
 
