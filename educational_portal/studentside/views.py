2419a829be31ed3cf84609b4ef210a89adea4b3c
[TrushalPatel0] <[business.trushalpatel@gmail.com]>
2024-11-23 14:10:04 +0530

    changes done

--- educational_portal/studentside/views.py
+++ educational_portal/studentside/views.py
@@ -170,6 +170,18 @@ def student_login_page(request):
     domain = request.get_host()
     Institute_data = NewInstitution.objects.get(institute_domain = domain)
     login=1
+    context = {}
+    if request.GET.get("deviceId"):
+        deviceId = request.GET.get('deviceId','1')
+        context.update({'deviceId':deviceId})
+        if deviceId != '123':
+            request.session['deviceId'] = deviceId
+        context.update({'deviceId':deviceId})
+    if request.GET.get("version"):
+        version = request.GET.get('version','1')  
+        context.update({'version':version})
+
+
     if request.COOKIES.get("stud_email"):
             cookie_email = request.COOKIES['stud_email'].lower()
             cookie_pass = request.COOKIES['stud_password']
