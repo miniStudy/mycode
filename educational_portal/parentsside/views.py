89648a929c66c001dcb17d0ce58aaa8c19beed22
anilkathayat07 <kathayatanil3@gmail.com>
2024-11-28 16:16:28 +0530

    Changes

--- educational_portal/parentsside/views.py
+++ educational_portal/parentsside/views.py
@@ -556,13 +556,14 @@ def parent_chatbox(request):
         )
         context.update({'selected_person':selected_person, 'chatbox_data': chatbox_data})
     else:
-        selected_person = Faculties.objects.get(fac_email = Persons[0]['chatbox_sender'])
-        chatbox_data = Chatbox.objects.filter( 
-        Q(chatbox_sender=parent_object.stud_guardian_email,chatbox_receiver = selected_person.fac_email) |
-        Q(chatbox_receiver=parent_object.stud_guardian_email, chatbox_sender = selected_person.fac_email)
-        )
-        context.update({'selected_person':selected_person, 'chatbox_data': chatbox_data})
-    
+        if Persons:
+            selected_person = Faculties.objects.get(fac_email = Persons[0]['chatbox_sender'])
+            chatbox_data = Chatbox.objects.filter( 
+            Q(chatbox_sender=parent_object.stud_guardian_email,chatbox_receiver = selected_person.fac_email) |
+            Q(chatbox_receiver=parent_object.stud_guardian_email, chatbox_sender = selected_person.fac_email)
+            )
+            context.update({'selected_person':selected_person, 'chatbox_data': chatbox_data})
+            
 
     Unique_persons = []
     for x in Persons:
